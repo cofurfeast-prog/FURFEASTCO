4ded0ce6e311a4e362883386e6f115798a45b374
cofurfeast <cofurfeast@gmail.com>
2026-01-19 12:33:51 +0000

    Fix Django module path and PYTHONPATH for nested structure

--- FURFEASTCO/start.sh
+++ FURFEASTCO/start.sh
@@ -17,3 +17,5 @@ python manage.py collectstatic --noinput
 
 # Start Gunicorn server with optimized settings for Cloud Run
 exec gunicorn FURFEASTCO.wsgi:application --bind 0.0.0.0:$PORT --workers 1 --timeout 300 --keep-alive 2 --max-requests 1000 --max-requests-jitter 50
+# Add to PYTHONPATH for Django to find modules
+export PYTHONPATH=/app:/app/FURFEASTCO:$PYTHONPATH
